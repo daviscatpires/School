f722bc4f6a1cabbd2d90747b47aa049410dcc3f1
daviscatpires <80295311+daviscatpires@users.noreply.github.com>
2023-04-25 15:47:52 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 const maxLength = 144;
 const text = document.getElementById('text');
 const count = document.getElementById('count');
+const OPENAI_API_KEY = "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"; //Chave da api
 
 function updateCount(){
     const currentLength = text.value.length;
@@ -15,8 +16,18 @@ let savedText = '';
 function saveText(){
     const text = document.getElementById('text');
     savedText = text.value;
-    console.log('Texto salvo:', savedText)
+    console.log('Texto salvo:', savedText);
+    main();
 }
+
+//Para enviar com enter
+const inputEle=document.getElementById('text');
+inputEle.addEventListener('keyup', function(e){
+    var key = e.which || e.keyCode;
+    if(key == 13){
+        saveText();
+    }
+});
 
 //Audio
 const recognition = new webkitSpeechRecognition();
@@ -28,7 +39,7 @@ recognition.interimResults = true;
 
 recognition.onresult = event =>{
     const transcript = event.results[event.results.length-1][0].transcript;
-    const transcriptonElement = document.getElementById('transcription');
+    const transcriptonElement = document.getElementById('text');
 
     transcriptonElement.textContent = transcript;
 };
@@ -43,48 +54,40 @@ function stopRecognition(){
     recognition.stop();
 }
 
-//OPEN AI
-const openai_secret_key = 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; //Chave da api
+//Requisição para o chatgpt
+async function main(){
+    console.log('Enviando prompt para a API do OpenAI:', prompt);
 
-//Função assíncrona para obter a resposta do ChatGPT
 
-async function getResponse(prompt){
     //Faz uma requisição para API do OpenAI
-    const response = await fetch('https://api.openai.com/v1/engines/davinci/completions',{
+    const response = await fetch('https://api.openai.com/v1/completions',{
         method:'POST',
         headers:{
-            'Content-Type':'application/json',
+            Accept: "application/json", "Content-Type":"application/json",
             //Adiciona a chave de API como parâmetro de autorização
-            'Authorization':`Bearer ${openai_secret_key}`
+            Authorization:`Bearer ${OPENAI_API_KEY}`
         },
         body: JSON.stringify({
+            //Modelo
+            model:"text-davinci-003",
             //Envia o prompt como parâmetro
-            prompt: prompt,
+            prompt: ("Vamos falar em inglês, corrija os erros de ingles, tanto gramatica como concordancia explicando o que esta errado e depois responda continuando o assunto:" + savedText),
             //Define o número máximo de tokens na resposta
-            max_tokens:150,
+            max_tokens:2048,
             //Define a temperatura da resposta(criatividade)
-            temperature:0.9,
-            //Define o número de respostas a serem geradas
-            n:1,
-            //Define o caractere de parada para a geração de teto
-            stop: null
-        })
-    });
-    //Converte a resposta em um objeto JSON
-    const data = await response.json();
-    //Retorna o texto da primeira resposta gerada
-    return data.choices[0].text;
-}
-
-//Função principal para testar a função getResponse
-async function main(){
-    //Define o prompt a ser enviado para o ChatGPT
-    const prompt = 'Whats is the capital of france?';
-    //Obtém a resposta do ChatGPT
-    const response = await getReponse(prompt);
-    //Exige a resposta no console
-    console.log(response);
-}
-
-//Chama a função principal
-main();
+            temperature:0.5,
+        }),
+    })
+    //Acessa o then quando tem resposta
+    .then((resposta)=>resposta.json())
+    .then((dados)=>{
+        //console.log(dados);
+        //console.log(dados.choices[0].text);
+        //Enviar o texto da resposta
+        document.getElementById('result').innerHTML = dados.choices[0].text;
+    })
+    //Retorna catch quando gerar algum erro
+    .catch((erro)=>{
+        console.log(erro);
+    })
+};
